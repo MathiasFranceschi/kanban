0769f2b190f0de8c85ca922e69cf32846a558ff6
MathiasFranceschi <mathias.franceschi@oclock.school>
2023-09-05 14:18:59 +0200

    static

--- index.js
+++ index.js
@@ -6,17 +6,18 @@ const router = require('./app/router.js');
 const cors = require('cors');
 
 app.use(cors('*'));
+app.use(express.static('dist'));
 // pour réagir aux formulaires, on rajoute ce middleware
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
 
-app.use(router);
 
 
 app.set('port', process.env.PORT || 5000);
 app.set('base_url', process.env.BASE_URL + ':' + app.get('port'));
 
+app.use(router);
 
 app.listen(app.get('port'), () => {
         console.log(`Listening on ${process.env.BASE_URL
